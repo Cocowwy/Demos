989b72fc5ff425e73489c2443918147e6a9f1eb2
Cocowwy <514658459@qq.com>
2022-04-13 14:21:30 +0800

    init graphql moudle

---
+++ graphql-java/src/main/java/cn/cocowwy/graphqljava/GraphqlJavaApplication.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.graphqljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlJavaApplication.class, args);
+    }
+
+}
---
+++ graphql-java/src/test/java/cn/cocowwy/graphqljava/GraphqlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.graphqljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
