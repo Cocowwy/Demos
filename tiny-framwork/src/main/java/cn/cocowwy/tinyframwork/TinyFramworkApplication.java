1ef0cb79ab79fb24b1fa6bf267fce6c8eec8577e
Cocowwy <514658459@qq.com>
2022-06-03 11:07:54 +0800

    add 庭院 framfork

---
+++ tiny-framwork/src/main/java/cn/cocowwy/tinyframwork/TinyFramworkApplication.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.tinyframwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyFramworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyFramworkApplication.class, args);
+    }
+
+}
---
+++ tiny-framwork/src/test/java/cn/cocowwy/tinyframwork/TinyFramworkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.tinyframwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyFramworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
