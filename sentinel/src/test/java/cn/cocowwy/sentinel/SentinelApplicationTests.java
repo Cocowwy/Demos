b3476ef58b8f367ecacdb70093bae533f097f6e2
Cocowwy <514658459@qq.com>
2022-06-29 18:30:02 +0800

    add sentinel test

---
+++ sentinel/src/test/java/cn/cocowwy/sentinel/SentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
