57086894ecbbd877356de95f284298c2fe0b3873
Cocowwy <514658459@qq.com>
2022-07-13 11:22:09 +0800

    sugar

---
+++ beautiful-code/src/main/java/cn/cocowwy/beautifulcode/sugar/Sugar.java
@@ -0,0 +1,15 @@
+package cn.cocowwy.beautifulcode.sugar;
+
+/**
+ * @author cocowwy.cn
+ * @create 2022-05-05-11:45
+ */
+public class Sugar {
+    public static void main(String[] args) {
+        // 分隔符，便于清楚的数位数
+        System.out.println(1_000_000);
+
+        // 2的三次方
+        System.out.println(1 << 3);
+    }
+}
