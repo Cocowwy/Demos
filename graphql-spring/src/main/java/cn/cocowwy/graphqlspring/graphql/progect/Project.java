72c53404fed0f00914ddc8c39b3816c371187084
Cocowwy <514658459@qq.com>
2022-07-26 16:21:49 +0800

    tiny update

--- graphql-spring/src/main/java/cn/cocowwy/graphqlspring/graphql/progect/Project.java
+++ graphql-spring/src/main/java/cn/cocowwy/graphqlspring/graphql/progect/Project.java
@@ -1,14 +1,12 @@
 package cn.cocowwy.graphqlspring.graphql.progect;
 
-import lombok.Data;
-
 import java.util.List;
 
 /**
  * @author cocowwy.cn
  * @create 2022-05-05-11:45
  */
-@Data
+
 public class Project {
     private String slug;
 
@@ -19,4 +17,44 @@ public class Project {
     private ProjectStatus status;
 
     private List<Release> releases;
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRepositoryUrl() {
+        return repositoryUrl;
+    }
+
+    public void setRepositoryUrl(String repositoryUrl) {
+        this.repositoryUrl = repositoryUrl;
+    }
+
+    public ProjectStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ProjectStatus status) {
+        this.status = status;
+    }
+
+    public List<Release> getReleases() {
+        return releases;
+    }
+
+    public void setReleases(List<Release> releases) {
+        this.releases = releases;
+    }
 }
--- graphql-spring/src/main/java/cn/cocowwy/graphqlspring/graphql/progect/Release.java
+++ graphql-spring/src/main/java/cn/cocowwy/graphqlspring/graphql/progect/Release.java
@@ -1,12 +1,9 @@
 package cn.cocowwy.graphqlspring.graphql.progect;
 
-import lombok.Data;
-
 /**
  * @author cocowwy.cn
  * @create 2022-05-05-11:45
  */
-@Data
 public class Release {
     private String version;
 
@@ -18,4 +15,43 @@ public class Release {
 
     private boolean current;
 
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public ReleaseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReleaseStatus status) {
+        this.status = status;
+    }
+
+    public String getReferenceDocUrl() {
+        return referenceDocUrl;
+    }
+
+    public void setReferenceDocUrl(String referenceDocUrl) {
+        this.referenceDocUrl = referenceDocUrl;
+    }
+
+    public String getApiDocUrl() {
+        return apiDocUrl;
+    }
+
+    public void setApiDocUrl(String apiDocUrl) {
+        this.apiDocUrl = apiDocUrl;
+    }
+
+    public boolean isCurrent() {
+        return current;
+    }
+
+    public void setCurrent(boolean current) {
+        this.current = current;
+    }
 }
