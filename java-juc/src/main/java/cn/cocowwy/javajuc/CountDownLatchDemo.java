c0e31da4a915a2244bc5af22c3102c742c76072f
Cocowwy <514658459@qq.com>
2022-07-27 19:04:34 +0800

    CountDownLatchDemo

--- java-juc/src/main/java/cn/cocowwy/javajuc/CountDownLatchDemo.java
+++ java-juc/src/main/java/cn/cocowwy/javajuc/CountDownLatchDemo.java
@@ -1,8 +1,10 @@
 package cn.cocowwy.javajuc;
 
+import cn.hutool.core.util.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author cocowwy.cn
@@ -15,17 +17,40 @@ public class CountDownLatchDemo {
 
         for (int i = 0; i < 10; i++) {
             int user = i;
-            new Thread(() -> {
+            Thread thread = new Thread(() -> {
                 try {
-                    Thread.sleep(3000L);
+                    // 模拟不同玩家超时的时间
+                    Thread.sleep(RandomUtil.randomLong(4000L, 6000L));
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-                log.info("玩家 【%s】加载完成", user);
-            }).start();
-            cdl.countDown();
+                // 这里设置成守护进程，即当超时了 就退出
+                log.info("玩家 【{}】加载完成", user);
+                cdl.countDown();
+            });
+            thread.setDaemon(true);
+            thread.start();
         }
 
-        log.info("加载完成，进入游戏");
+        boolean res = false;
+        try {
+            res = cdl.await(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            // ignore..
+        }
+        if (res) {
+            log.info("加载完成，进入游戏......");
+        } else {
+            log.info("加载失败，玩家超时......");
+        }
+    }
+}
+
+/**
+ * 实现功能  同时从多个数据源进行请求，先返回数据就继续往后执行代码
+ */
+class CountDownLatchDemo1 {
+    public static void main(String[] args) {
+
     }
 }
