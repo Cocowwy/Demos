11acbc4bc9c4413d4d8964e51e6ac15990e2c779
Cocowwy <514658459@qq.com>
2022-08-04 15:56:53 +0800

    arthas study

---
+++ arthas/src/main/java/cn/cocowwy/arthas/ArthasApplication.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.arthas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArthasApplication.class, args);
+    }
+
+}
