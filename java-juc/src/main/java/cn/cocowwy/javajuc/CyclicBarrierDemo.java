f30c1fe10c117470a752f2800cd5c7c5e106d418
Cocowwy <514658459@qq.com>
2022-08-10 18:46:16 +0800

    CyclicBarrier study

--- java-juc/src/main/java/cn/cocowwy/javajuc/CyclicBarrierDemo.java
+++ java-juc/src/main/java/cn/cocowwy/javajuc/CyclicBarrierDemo.java
@@ -1,29 +1,40 @@
 package cn.cocowwy.javajuc;
 
+import cn.hutool.core.util.RandomUtil;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 /**
+ * CyclicBarrier可以使一定数量的线程反复地在栅栏位置处汇集。当线程到达栅栏位置时将调用await方法，
+ * 这个方法将阻塞直到所有线程都到达栅栏位置。如果所有线程都到达栅栏位置，那么栅栏将打开，此时所有的线程都将被释放，
+ * 而栅栏将被重置以便下次使用。
+ *
  * @author cocowwy.cn
  * @create 2022-05-05-11:45
  */
+@Slf4j
 public class CyclicBarrierDemo {
     @SneakyThrows
     public static void main(String[] args) {
-        CyclicBarrier cbr = new CyclicBarrier(2, () -> {
-            System.out.println("CyclicBarrier right");
+        CyclicBarrier cbr = new CyclicBarrier(10, () -> {
+            System.out.println("CyclicBarrier is open");
         });
+
         for (int i = 0; i < 10; i++) {
             new Thread(() -> {
+                String name = Thread.currentThread().getName();
                 try {
-                    cbr.await();
+                    Thread.sleep(RandomUtil.randomLong(1000L, 3000L));
+                    System.out.println(name + " 到达栅栏，进行阻塞...");
+                    cbr.await(5L, TimeUnit.SECONDS);
+                    System.out.println(name + " 阻塞完成");
                 } catch (Exception e) {
-                    throw new RuntimeException(e);
+                    log.error(name + "等待栅栏开放超时");
                 }
-            }).start();
+            }, "线程" + i).start();
         }
-
-        System.out.println("主线程～～～");
     }
 }
