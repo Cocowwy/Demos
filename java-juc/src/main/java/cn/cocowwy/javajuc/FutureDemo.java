768573cf2ce6eed452fb38a7a89db3fa8ebb79d4
Cocowwy <514658459@qq.com>
2022-08-14 10:27:02 +0800

    sutdy future

--- java-juc/src/main/java/cn/cocowwy/javajuc/FutureDemo.java
+++ java-juc/src/main/java/cn/cocowwy/javajuc/FutureDemo.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
- * https://blog.csdn.net/qq_31865983/article/details/106137777?spm=1001.2101.3001.6661.1&utm_medium=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-106137777-blog-89756110.t0_layer_eslanding_sa&depth_1-utm_source=distribute.pc_relevant_t0.none-task-blog-2%7Edefault%7ECTRLIST%7ERate-1-106137777-blog-89756110.t0_layer_eslanding_sa&utm_relevant_index=1
  * @author cocowwy.cn
  * @create 2022-05-05-11:45
  */
