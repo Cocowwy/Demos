29af9c6541d03f86a1f1013e1e610fd76f6176b0
Cocowwy <514658459@qq.com>
2022-08-15 10:03:28 +0800

    gitignore

---
+++ graphql-spring/src/main/java/cn/cocowwy/graphqlspring/GraphqlSpringApplication.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.graphqlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringApplication.class, args);
+    }
+
+}
