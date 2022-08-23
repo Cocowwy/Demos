e17bf5d6faf093c9dc23fc62291059e1e845bcd7
Cocowwy <514658459@qq.com>
2022-08-23 18:58:55 +0800

    arthas study

--- arthas/src/main/java/cn/cocowwy/arthas/controller/TestController.java
+++ arthas/src/main/java/cn/cocowwy/arthas/controller/TestController.java
@@ -5,13 +5,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * cd /Users/cocowwy/Desktop/space/idea-space/Demos/arthas/src/main/resources/static
+ * 启动arthas： java -jar arthas-boot.jar
+ *
  * @author cocowwy.cn
  * @create 2022-05-05-11:45
  */
 @RestController
 public class TestController {
-    private volatile boolean cycle = true;
+    private boolean cycle = true;
 
+    /**
+     * curl localhost:8080/test
+     * @return
+     */
     @GetMapping("/test")
     public String test() {
         Thread.currentThread().setName("test" + System.currentTimeMillis());
@@ -22,9 +29,24 @@ public class TestController {
         return "test";
     }
 
+    /**
+     * curl localhost:8080/label/false
+     * @param data
+     * @return
+     */
     @GetMapping("/label/{data}")
-    public String out(@PathVariable("data")Boolean data) {
+    public String out(@PathVariable("data") Boolean data) {
         cycle = data;
         return "test";
+    }
+
+    /**
+     * curl localhost:8080/gc
+     * @return
+     */
+    @GetMapping("/gc")
+    public String gc() {
+        System.gc();
+        return "OK";
     }
 }
