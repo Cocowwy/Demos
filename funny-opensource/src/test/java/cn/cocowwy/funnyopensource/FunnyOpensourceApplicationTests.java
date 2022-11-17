744f0f6f3b05b6b421305dd3e061a1993ec2f8b9
Cocowwy <514658459@qq.com>
2022-11-17 10:27:33 +0800

    🍓 有趣的java代码

---
+++ funny-opensource/src/test/java/cn/cocowwy/funnyopensource/FunnyOpensourceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.funnyopensource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyOpensourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
