a7771e7accb703dccd391f05f53bb20712b376de
Cocowwy <514658459@qq.com>
2023-01-04 15:13:29 +0800

    🍓 transcation util

--- spring/src/main/java/cn/cocowwy/spring/transaction/TransactionHelper.java
+++ spring/src/main/java/cn/cocowwy/spring/transaction/TransactionHelper.java
@@ -1,6 +1,11 @@
 package cn.cocowwy.spring.transaction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -8,7 +13,24 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
  * @author <a href="https://github.com/Cocowwy">Cocowwy</a>
  * @since 2023/1/4
  */
+@Component
 public class TransactionHelper {
+    @Autowired
+    private DataSourceTransactionManager dataSourceTransactionManager;
+
+    public TransactionStatus begin() {
+        TransactionStatus transaction = dataSourceTransactionManager.
+                getTransaction(new DefaultTransactionAttribute());
+        return transaction;
+    }
+
+    public void commit(TransactionStatus transaction) {
+        dataSourceTransactionManager.commit(transaction);
+    }
+
+    public void rollback(TransactionStatus transaction) {
+        dataSourceTransactionManager.rollback(transaction);
+    }
 
     /**
      * 使用此方法的位置必须要有 {@link Transactional} 注解
